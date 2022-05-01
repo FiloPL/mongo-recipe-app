f850e06f4560c5f8e74bfda76bc40ba63c730505
Tomasz Zegarlicki <Tomasz.Zegarlicki@ttsw.com.pl>
2022-05-01 12:46:33 +0200

    Initial commit

---
+++ src/test/java/filo/springapp/mongorecipeapp/MongoRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package filo.springapp.mongorecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
